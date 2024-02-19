8e644d66ef6ff2adb4ee43ff1b1d1a632004798c
edvard01 <frejaclueless@gmail.com>
2024-02-19 16:20:38 +0100

    First commit, added navbar component and imported css file

---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+import "./nav.css";
+
+export function Nav() {
+  return (
+    <navbar className="navbar">
+      <nav>
+        <a href="#">link 1</a>
+        <a href="#">link 2</a>
+        <a href="#">link 3</a>
+      </nav>
+    </navbar>
+  );
+}
