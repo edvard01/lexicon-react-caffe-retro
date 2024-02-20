1dc1c1ede48da689dd9a9f5aa7272590a25f6f5c
edvard01 <frejaclueless@gmail.com>
2024-02-20 11:17:28 +0100

    Started styling and made the first panel

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,9 +4,9 @@ export function Nav() {
   return (
     <div className="navbar">
       <nav>
-        <a href="#">link 1</a>
-        <a href="#">link 2</a>
-        <a href="#">link 3</a>
+        <a href="#">Hot</a>
+        <a href="#">Juicy</a>
+        <a href="#">Cosy</a>
       </nav>
     </div>
   );
--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,3 +1,21 @@
+import "./panel.css";
+
 export function Panel() {
-  return <div className="">This is the content of a panel</div>;
+  return (
+    <div className="panel">
+      <div className="content">
+        <h4>Hot</h4>
+        <p className="text-box box-1">
+          Hot freshly ground black coffee or a cup of exquisite tea?
+        </p>
+        <p className="text-box box-2">
+          We give you that perfect cup every time
+        </p>
+        <div className="price-box">
+          <span className="drinks"></span>
+          <span className="prices"></span>
+        </div>
+      </div>
+    </div>
+  );
 }
